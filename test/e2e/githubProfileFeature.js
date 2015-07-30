8fdf608fb2dc435f20ca47f8baca0916dff8770e
Paul Harker <harkerp@gmail.com>
2015-07-30 10:44:38 +0100

    adds browser tests for firefox and chrome

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,8 +1,21 @@
 describe('Github profile finder', function() {
-  it('has a title', function() {
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function(){
     browser.get('http://localhost:8080');
+  })
+
+  it('has a title', function() {
+
 
     expect(browser.getTitle()).toEqual('Github user search');
   });
-  
+  it ('finds profiles', function(){
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+
+    expect(element(by.binding('user.login')).getText()).
+      toEqual('spike01')
+  });
 });
