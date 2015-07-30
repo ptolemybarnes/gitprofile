c4b18fb9f27cb18bbae3c9eab7f3d527f69e0db4
Paul Harker <harkerp@gmail.com>
2015-07-30 14:08:42 +0100

    adds extra amazing tests which I'm really happy with. Great triangulation

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -7,10 +7,9 @@ describe('Github profile finder', function() {
   })
 
   it('has a title', function() {
-
-
     expect(browser.getTitle()).toEqual('Github user search');
   });
+
   it ('finds one profile', function(){
     searchBox.sendKeys('spike');
     searchButton.click();
@@ -19,6 +18,24 @@ describe('Github profile finder', function() {
       expect(list.last().getText()).toEqual(profiles.last().getText());
   });
 
-
-
+  it ('each returned item should have an image', function(){
+    searchBox.sendKeys('spike');
+    searchButton.click();
+    var counts;
+    element.all(by.repeater('user in searchCtrl.searchResult.items')).count().then(function(count) {
+      counts = count
+    var image = element.all(by.css('img'));
+    expect(image.count()).toEqual(counts);
+    });
+  })
+  it ('each returned item should have a link', function(){
+    searchBox.sendKeys('spike');
+    searchButton.click();
+    var counts;
+    element.all(by.repeater('user in searchCtrl.searchResult.items')).count().then(function(count) {
+      counts = count
+    var link = element.all(by.css('ul a'));
+    expect(link.count()).toEqual(counts);
+    });
+  });
 });
